17057d30e189bc264f3f13781631b16cb8d3ac56
daniu54 <daniu.coding@proton.me>
2024-12-19 13:43:32 +0100

    cleaned up imports

--- ModelsPlayground/pipeline.py
+++ ModelsPlayground/pipeline.py
@@ -7,11 +7,6 @@ import numpy as np
 
 import uuid
 
-# models
-from scipy.optimize import curve_fit  # multip. linear regression
-from sklearn.svm import SVR  # support vector forrest
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.neural_network import MLPRegressor
 from sklearn.model_selection import train_test_split, cross_val_score
 from sklearn.metrics import mean_squared_error, r2_score
 
@@ -21,8 +16,6 @@ import traceback
 import pandas as pd
 
 import joblib  # for persisting models
-
-from enum import Enum
 
 from pathlib import Path
 
