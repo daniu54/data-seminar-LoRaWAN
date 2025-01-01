a0b4a676c482444ecc2d82f794c8f28600c9908d
daniu54 <daniu.coding@proton.me>
2025-01-01 20:08:36 +0100

    changed id in results, added other models

--- PredictionPathLossUsingML/model_tests/best_using_param_search.py
+++ PredictionPathLossUsingML/model_tests/best_using_param_search.py
@@ -3,11 +3,6 @@ import os
 import uuid
 
 # models
-from scipy.optimize import curve_fit # multip. linear regression
-from sklearn.svm import SVR # support vector forrest
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.neural_network import MLPRegressor
-
 import pandas as pd
 
 # quick-and-dirty import, since I could not get relative imports to work
@@ -22,7 +17,6 @@ data_path = ".." / data_path
 
 stats_file = pipeline.DEFAULT_STATS_FILE
 stats_file = ".." / stats_file
-# stats_file = Path("../results/model_stats copy.json")
 
 data = pipeline.load_data(data_path)
 data = pipeline.clean_data(data)
@@ -30,7 +24,6 @@ data = pipeline.add_features(data)
 
 print(data.columns)
 print(pipeline.calculate_correlation(data))
-
 
 from sklearn.ensemble import AdaBoostRegressor
 from sklearn.tree import DecisionTreeRegressor
@@ -41,18 +34,54 @@ ada_boost = AdaBoostRegressor(
     learning_rate=0.6903938978839036
 )
 
+from xgboost import XGBRegressor
+
+xg_boost = XGBRegressor(max_depth= 15,
+    learning_rate=0.019501791550458624,
+    n_estimators= 436,
+    gamma= 2.5671387248762607,
+    min_child_weight= 5,
+    subsample= 0.5728919054154519,
+    colsample_bytree= 0.889193793406033,
+    reg_alpha=1.4832156197353648,
+    reg_lambda= 5.336603614464974)
+
+
+from sklearn.ensemble import GradientBoostingRegressor
+
+gradient_boosting_regressor = GradientBoostingRegressor(
+        n_estimators=500,
+        learning_rate=0.05,
+        max_depth=3,
+        random_state=42
+    )
+
 skip_on_duplicate = True
+save_model = True
 
 test_specifications = [
     {
-        'id': f"optimal_ada_boost_{1}",
-        # 'model': "models/optimal_ada_boost_1.joblib",
+        'id': f"ada_boost_{1}",
         'model': ada_boost,
-        # 'skip_fitting': True,
-        # "skip_cross_validation": False
         'verbose': 10,
         'n_jobs': -1,
-        'save_model': True,
+        'save_model': save_model,
+        'skip_on_duplicate': skip_on_duplicate
+    },
+    {
+        'id': f"xg_boost_{1}",
+        'model': xg_boost,
+        'verbose': 10,
+        'n_jobs': -1,
+        'save_model': save_model,
+        'skip_on_duplicate': skip_on_duplicate
+    },
+    {
+        'id': f"gradient_boosting_regressor_{1}",
+        'model': gradient_boosting_regressor,
+        'verbose': 10,
+        'n_jobs': -1,
+        'save_model': save_model,
         'skip_on_duplicate': skip_on_duplicate
     },
 ]
